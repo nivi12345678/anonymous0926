e72d8e23d68876ff7af53a6aa7909634aa58aec5
ano <nivethitha0901@gmail.com>
2026-08-07 18:51:34 +0530

    Update softmax_unit_w8_reg.v

--- softmax_unit_w8_reg.v
+++ softmax_unit_w8_reg.v
@@ -20,7 +20,7 @@ module softmax_unit #(
     end
     localparam signed [31:0] LO_SWAT=-1024, LO_STABLE=-2048;
     localparam integer SPAN_SHIFT=11;
-    localparam signed [31:0] GATE=3072;
+    localparam signed [31:0] GATE=768;
 
     reg [WIN*W-1:0] scores_r;  reg start_r;
     always @(posedge clk) begin
